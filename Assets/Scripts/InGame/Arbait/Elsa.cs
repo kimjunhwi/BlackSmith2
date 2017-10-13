e71d34156b6cd2f65f22b4ca058e885b09a14362
kimjunhwi <gustlraks@naver.com>
2017-10-13 16:13:48 +0900

    asd

--- Assets/Scripts/InGame/Arbait/Elsa.cs
+++ Assets/Scripts/InGame/Arbait/Elsa.cs
@@ -56,7 +56,7 @@ public class Elsa : ArbaitBatch {
 	public override void Setting ()
 	{
 		m_CharacterChangeData.name = name;
-		m_CharacterChangeData.strExplains = string.Format ("물 사용시 모든 직원 공격속도 {0:F1}% 증가", m_CharacterChangeData.fSkillPercent);
+		m_CharacterChangeData.strExplains = string.Format ("물 사용시 모든 직원 수리력 {0:F1}% 증가", m_CharacterChangeData.fSkillPercent);
 		m_CharacterChangeData.strPurchasing = string.Format ("대장간 등급 3");
 	}
 
@@ -82,7 +82,7 @@ public class Elsa : ArbaitBatch {
 	{
 		m_CharacterChangeData.fSkillPercent = m_CharacterChangeData.fSkillPercent + 0.2f;
 
-		m_CharacterChangeData.strExplains = string.Format ("물 사용시 모든 직원 공격속도 {0:F1}% 증가", m_CharacterChangeData.fSkillPercent);
+		m_CharacterChangeData.strExplains = string.Format ("물 사용시 모든 직원 수리력 {0:F1}% 증가", m_CharacterChangeData.fSkillPercent);
 	}
 
 	public override IEnumerator AuraParticle ()
